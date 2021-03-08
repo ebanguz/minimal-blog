43779fd7c60c330dc0e1b1b7965a673e87041e09
Esteban Guzmán <57820858+ebanguz@users.noreply.github.com>
2021-03-08 17:48:53 -0400

    feat: responsive

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ window.addEventListener("scroll", () => {
 	const header = document.querySelector("header");
 	header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+const hamburger = document.querySelector(".hamburguer");
+const navUL = document.querySelector(".nav-ul");
+
+hamburger.addEventListener("click", () => {
+	navUL.classList.toggle("show");
+});
+
+const vacio = document.querySelector(".block-container");
+
+vacio.addEventListener("click", () => {
+	if (navUL.classList.contains("show")) {
+		navUL.classList.toggle("show");
+	}
+});
